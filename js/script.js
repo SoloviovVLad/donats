2679bd3c3b88c237406b0f93ca9292d7eec4eab8
Vlad <4solovya@gmail.com>
2022-01-15 17:54:09 +0200

    push block donats

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// 
+if (jQuery(window).width() > 768) {
+    function parallax(event){
+        this.querySelectorAll(".parallax").forEach(parallax => {
+            parallax.style.transform = `translateX(${event.clientX/40}px)`
+        });
+    }
+
+    document.addEventListener('mousemove', parallax);
+}
